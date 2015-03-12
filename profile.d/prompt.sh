29c3f1051011842e219e2c444516067b29eaa1a6
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-03-12 21:53:40 +0900

    prompt.sh をプロファイル用ディレクトリへ格納

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 export PS1='\n\[\e[32m\]\u@\h\[\e[0m\]:\[\e[36m\]\w $(c=$?;e="\[\e[34m\]";if [ $c != 0 ];then e="\[\e[31m\]";fi;echo $e[$c])\[\e[33m\]$(__git_ps1 2>/dev/null)\[\e[0m\]\n\$ '
