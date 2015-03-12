efe07e54ae615f7716d99c3911b34ddd840bfae3
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-03-12 22:36:31 +0900

    インストールスクリプトのメッセージを分かりやすく

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 # インストール
 ########################################################################################################################
 
-set -eux
+set -eu
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # ファンクション: ファイル "~/.dotfiles" を作成
-function make_dotdotfiles() {
-    pushd $(dirname $0)
+function create_dotdotfiles() {
+    pushd $(dirname $0) >/dev/null
+    echo "create: ${HOME}/.dotfiles (DOTFILES_HOME=$PWD)"
     echo "export DOTFILES_HOME=$PWD" >$HOME/.dotfiles
-    popd
+    popd >/dev/null
+    . ${HOME}/.dotfiles
 }
 
 # ファンクション: シンボリックリンクを作成, 既存ファイルは退避
@@ -21,19 +23,20 @@ function link_dotfile() {
     to_dir=$2
     from_file=$DOTFILES_HOME/dotfiles/$file_name
     to_file=$to_dir/$file_name
-    backup_dir=$DOTFILES_HOME/backup/$to_dir
-    backup_file=$DOTFILES_HOME/backup/$to_file.$timestamp
+    backup_dir=$DOTFILES_HOME/backup/${to_dir#/}
+    backup_file=$DOTFILES_HOME/backup/${to_file#/}.$timestamp
     if [ -e $to_file -o -L $to_file ]; then
+        echo "move: $to_file => $backup_file"
         mkdir -p $backup_dir
         mv $to_file $backup_file
     fi
+    echo "link: $from_file => $to_file"
     mkdir -p $to_dir
     ln -s $from_file $to_file
 }
 
 # メイン処理
-make_dotdotfiles
-. ${HOME}/.dotfiles
-link_dotfile .bash_profile  ${HOME}
-link_dotfile .gitconfig     ${HOME}
-link_dotfile .gitattributes ${HOME}
+create_dotdotfiles
+link_dotfile .bash_profile  $HOME
+link_dotfile .gitconfig     $HOME
+link_dotfile .gitattributes $HOME
