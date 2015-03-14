8848bc015d519858c7bb523f93fa7e17ff23a35d
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-03-15 04:35:35 +0900

    PS1 かわいさ UP!!!

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -1,1 +1,27 @@
-export PS1='\n\[\e[32m\]\u@\h\[\e[0m\]:\[\e[36m\]\w $(c=$?;e="\[\e[34m\]";if [ $c != 0 ];then e="\[\e[31m\]";fi;echo $e[$c])\[\e[33m\]$(__git_ps1 2>/dev/null)\[\e[0m\]\n\$ '
+# Colors
+_ps1_red="\e[31m"
+_ps1_green="\e[32m"
+_ps1_yellow="\e[33m"
+_ps1_blue="\e[34m"
+_ps1_purple="\e[35m"
+_ps1_cyan="\e[36m"
+_ps1_reset="\e[00m"
+
+# PS1 parts
+function _ps1_result() {
+    code=$?
+    if [ ${code} == 0 ]; then
+        echo -e "${_ps1_blue}"'(っ*´∀`*)っ OK!!'" [${code}]${_ps1_reset}"
+    else
+        echo -e "${_ps1_red}"'(｡´･ω･`) NG...'" [${code}]${_ps1_reset}"
+    fi
+}
+_ps1_user="${_ps1_green}\u@\h${_ps1_reset}"
+_ps1_dir="${_ps1_cyan}\w${_ps1_reset}"
+function _ps1_git() {
+    echo -e "${_ps1_yellow}$(__git_ps1 2>/dev/null)${_ps1_reset}"
+}
+_ps1_prompt="\$ "
+
+# PS1
+export PS1="\n\$(_ps1_result)\n${_ps1_user}:${_ps1_dir}\$(_ps1_git)\n${_ps1_prompt}"
