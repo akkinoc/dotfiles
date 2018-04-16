7abc3e1f62d42cb8f0fb9ab60e52399b8512d067
Akihiro Kondo <akihyro@gmail.com>
2018-04-16 15:51:34 +0900

    Update ls color

--- .bash_profile.d/ls.sh
+++ .bash_profile.d/ls.sh
@@ -1,9 +1,9 @@
 if ls --color >/dev/null 2>&1; then
     alias ls="ls --color=auto"
-    export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;42:ow=37;42"
+    export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
 else
     alias ls="ls -G"
-    export LSCOLORS="exgxfxdxbxdxdxabhbachc"
+    export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
 fi
 alias l="ls -l"
 alias la="ls -al"
