63ca182c426e6bf932b36af51f88b25d4dd2a3d1
Akihiro Kondo <akihyro@gmail.com>
2018-04-16 20:46:32 +0900

    Shorten redirect syntax

--- .bash_profile.d/ls.sh
+++ .bash_profile.d/ls.sh
@@ -1,4 +1,4 @@
-if ls --color >/dev/null 2>&1; then
+if ls --color &>/dev/null; then
     export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
     alias ls="ls --color=auto"
 else
