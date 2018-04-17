91994b9dbadb229819893ccac7917a018fd96c5e
Akihiro Kondo <akihyro@gmail.com>
2018-04-17 09:08:17 +0900

    Update cd profile

--- .bash_profile.d/cd.sh
+++ .bash_profile.d/cd.sh
@@ -1,5 +1,11 @@
-alias @="cd"
+alias c="cd"
+alias ~="cd ~"
 alias -- -="cd -"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+function @() {
+    cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
