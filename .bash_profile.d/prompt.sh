9a6f8c776ce955a311e5744b31e188b832e98724
Akihiro Kondo <akihyro@gmail.com>
2018-04-23 12:16:35 +0900

    Update bash prompt

--- .bash_profile.d/prompt.sh
+++ .bash_profile.d/prompt.sh
@@ -1,41 +1,36 @@
 function _prompt_command() {
     local result=$?
     PS1="\[\e]0;\u at \h in \W\a\]"
-    PS1+="\n"
     if [[ $result -eq 0 ]]; then
-        PS1+="\[\e[32m\]✔ \\\$? = $result\[\e[m\]"
+        PS1+="\n\[\e[32m\]✔ \\\$? = $result\[\e[m\]"
     else
-        PS1+="\[\e[31m\]✘ \\\$? = $result\[\e[m\]"
+        PS1+="\n\[\e[31m\]✘ \\\$? = $result\[\e[m\]"
     fi
-    PS1+="\n"
     if [[ $USER != "root" ]]; then
-        PS1+="\[\e[35m\]\u\[\e[m\]"
+        PS1+="\n\[\e[35m\]\u\[\e[m\]"
     else
-        PS1+="\[\e[1;35m\]\u\[\e[m\]"
+        PS1+="\n\[\e[1;35m\]\u\[\e[m\]"
     fi
-    PS1+=" at "
     if [[ -z $SSH_CONNECTION ]]; then
-        PS1+="\[\e[36m\]\h\[\e[m\]"
+        PS1+=" at \[\e[36m\]\h\[\e[m\]"
     else
-        PS1+="\[\e[1;36m\]\H\[\e[m\]"
+        PS1+=" at \[\e[1;36m\]\H\[\e[m\]"
     fi
-    PS1+=" in "
     if [[ $PWD/ = $HOME/* ]]; then
-        PS1+="\[\e[34m\]\w\[\e[m\]"
+        PS1+=" in \[\e[34m\]\w\[\e[m\]"
     else
-        PS1+="\[\e[1;34m\]\w\[\e[m\]"
+        PS1+=" in \[\e[1;34m\]\w\[\e[m\]"
     fi
     if type -t __git_ps1 &>/dev/null; then
         PS1+="$(__git_ps1 " on \[\e[33m\]%s\[\e[m\]")"
     fi
-    PS1+="\n"
-    PS1+="\\\$ "
+    PS1+="\n\\\$ "
     return $result
 }
 
 PROMPT_COMMAND="_prompt_command"
 PROMPT_DIRTRIM=0
-PS1="\n\u at \H in \w\n\\\$ "
+PS1="\n\u at \h in \w\n\\\$ "
 PS2="> "
 PS3="#? "
 PS4="+ "
