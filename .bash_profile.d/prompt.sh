8ec6992b694c4850d173b3a673222d8a72056875
Akihiro Kondo <akihyro@gmail.com>
2018-04-23 22:12:33 +0900

    Support pipe status

--- .bash_profile.d/prompt.sh
+++ .bash_profile.d/prompt.sh
@@ -1,31 +1,65 @@
 function _prompt_command() {
-    local result=$?
-    PS1='\[\e]0;\u at \h in \W\a\]'
-    if [[ $result -eq 0 ]]; then
-        PS1+='\n\[\e[32m\]✔ \\$? = $result\[\e[m\]'
-    else
-        PS1+='\n\[\e[31m\]✘ \\$? = $result\[\e[m\]'
-    fi
+    local -a statuses=(${PIPESTATUS[@]})
+    PS1="$(_ps1_title)"
+    PS1+="$(_ps1_result "${statuses[@]}")"
+    PS1+="$(_ps1_user)"
+    PS1+="$(_ps1_host)"
+    PS1+="$(_ps1_wd)"
+    PS1+="$(_ps1_git)"
+    PS1+="$(_ps1_prompt)"
+}
+
+function _ps1_title() {
+    printf "%s" '\[\e]0;\u at \h in \W\a\]'
+}
+
+function _ps1_result() {
+    local -a statuses=($@)
+    printf "%s" '\n'
+    local -i index
+    for index in ${!statuses[@]}; do
+        local -i status=${statuses[$index]}
+        [[ $index -eq 0 ]] || printf "%s" ' | '
+        if [[ $status -eq 0 ]]; then
+            printf "%s [%d]%s" '\[\e[32m\]✔' "$status" '\[\e[m\]'
+        else
+            printf "%s [%d]%s" '\[\e[31m\]✘' "$status" '\[\e[m\]'
+        fi
+    done
+}
+
+function _ps1_user() {
     if [[ "$USER" != "root" ]]; then
-        PS1+='\n\[\e[35m\]\u\[\e[m\]'
+        printf "%s" '\n\[\e[35m\]\u\[\e[m\]'
     else
-        PS1+='\n\[\e[1;35m\]\u\[\e[m\]'
+        printf "%s" '\n\[\e[1;35m\]\u\[\e[m\]'
     fi
-    if [[ -z "$SSH_CONNECTION" ]]; then
-        PS1+=' at \[\e[36m\]\h\[\e[m\]'
+}
+
+function _ps1_host() {
+    if [[ -z "${SSH_CONNECTION:-}" ]]; then
+        printf "%s" ' at \[\e[36m\]\h\[\e[m\]'
     else
-        PS1+=' at \[\e[1;36m\]\H\[\e[m\]'
+        printf "%s" ' at \[\e[1;36m\]\H\[\e[m\]'
     fi
+}
+
+function _ps1_wd() {
     if [[ "$PWD/" == "$HOME/"* ]]; then
-        PS1+=' in \[\e[34m\]\w\[\e[m\]'
+        printf "%s" ' in \[\e[34m\]\w\[\e[m\]'
     else
-        PS1+=' in \[\e[1;34m\]\w\[\e[m\]'
+        printf "%s" ' in \[\e[1;34m\]\w\[\e[m\]'
     fi
+}
+
+function _ps1_git() {
     if type -t __git_ps1 &>/dev/null; then
-        PS1+="$(__git_ps1 ' on \[\e[33m\]%s\[\e[m\]')"
+        __git_ps1 ' on \[\e[33m\]%s\[\e[m\]'
     fi
-    PS1+='\n\$ '
-    return $result
+}
+
+function _ps1_prompt() {
+    printf "%s" '\n\$ '
 }
 
 PROMPT_COMMAND="_prompt_command"
