507a4be235404d0dbf191126ef1c2d8e7d8b22b7
Akihiro Kondo <akihyro@gmail.com>
2018-04-25 05:59:18 +0900

    Refactor brew file loading

--- .bash_profile.d/sources.sh
+++ .bash_profile.d/sources.sh
@@ -8,8 +8,7 @@ function _akihyro_dotfiles_sources_source_from_brew_if_exists {
     local file="$1"
     local args=("${@:2}")
     type -t brew &>/dev/null || return 0
-    local prefix="$(brew --prefix)"
-    [[ -z "$prefix" ]] || _akihyro_dotfiles_sources_source_if_exists "$prefix/$file" "${args[@]}"
+    _akihyro_dotfiles_sources_source_if_exists "$(brew --prefix)/$file" "${args[@]}"
 }
 
 _akihyro_dotfiles_sources_source_from_brew_if_exists "etc/profile.d/bash_completion.sh"
