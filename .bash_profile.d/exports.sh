0369a7c55873d2434f934a64cafb5de8aa7c0642
Akihiro Kondo <akihyro@gmail.com>
2018-04-26 18:54:21 +0900

    Update history time format

--- .bash_profile.d/exports.sh
+++ .bash_profile.d/exports.sh
@@ -1,5 +1,5 @@
 export HISTSIZE=10000
-export HISTTIMEFORMAT="%m/%d %H:%M:%S "
+export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 
 export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
 export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
