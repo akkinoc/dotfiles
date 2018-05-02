9c273112e9e13e6ae6983338bbf66f6d6d46373d
Akihiro Kondo <akihyro@gmail.com>
2018-05-02 21:35:13 +0900

    Export GREP_COLORS

--- .bash_profile.d/exports.sh
+++ .bash_profile.d/exports.sh
@@ -2,15 +2,14 @@ if type -t gpg-agent tty &>/dev/null; then
     export GPG_TTY="$(tty)"
 fi
 
+export GREP_COLOR="1;31"
+export GREP_COLORS="ms=1;31:mc=1;31:fn=35:ln=32:bn=32:se=36"
+
 export HISTSIZE=100000
 export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 
-if ls -d --color=auto &>/dev/null || type -t gls &>/dev/null; then
-    export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
-fi
-if ls -Gd &>/dev/null; then
-    export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
-fi
+export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
+export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
 
 if [[ ":$PATH:" != *:"$HOME/.dotfiles/bin":* ]]; then
     export PATH+=":$HOME/.dotfiles/bin"
