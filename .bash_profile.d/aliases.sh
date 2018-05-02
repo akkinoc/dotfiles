3fb45301b1efba584afe024800a85c76cd313634
Akihiro Kondo <akihyro@gmail.com>
2018-05-02 21:35:12 +0900

    Don't define aliases/variables if there is no original commands

--- .bash_profile.d/aliases.sh
+++ .bash_profile.d/aliases.sh
@@ -4,23 +4,43 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias -- -='cd -'
-alias @='cd "$(osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)")"'
+if type -t osascript &>/dev/null; then
+    alias @='cd "$(osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)")"'
+fi
 
 if type -t hub &>/dev/null; then
     alias g='hub'
-else
+elif type -t git &>/dev/null; then
     alias g='git'
 fi
 
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias zgrep='zgrep --color=auto'
-alias zegrep='zegrep --color=auto'
-alias zfgrep='zfgrep --color=auto'
-alias ggrep='ggrep --color=auto'
-alias gegrep='gegrep --color=auto'
-alias gfgrep='gfgrep --color=auto'
+if type -t grep &>/dev/null; then
+    alias grep='grep --color=auto'
+fi
+if type -t egrep &>/dev/null; then
+    alias egrep='egrep --color=auto'
+fi
+if type -t fgrep &>/dev/null; then
+    alias fgrep='fgrep --color=auto'
+fi
+if type -t zgrep &>/dev/null; then
+    alias zgrep='zgrep --color=auto'
+fi
+if type -t zegrep &>/dev/null; then
+    alias zegrep='zegrep --color=auto'
+fi
+if type -t zfgrep &>/dev/null; then
+    alias zfgrep='zfgrep --color=auto'
+fi
+if type -t ggrep &>/dev/null; then
+    alias ggrep='ggrep --color=auto'
+fi
+if type -t gegrep &>/dev/null; then
+    alias gegrep='gegrep --color=auto'
+fi
+if type -t gfgrep &>/dev/null; then
+    alias gfgrep='gfgrep --color=auto'
+fi
 
 alias hist='history'
 
@@ -29,7 +49,9 @@ if ls -d --color=auto &>/dev/null; then
 elif ls -Gd &>/dev/null; then
     alias ls='ls -G'
 fi
-alias gls='gls --color=auto'
+if type -t gls &>/dev/null; then
+    alias gls='gls --color=auto'
+fi
 alias l='ls -l'
 alias la='ls -al'
 
@@ -37,4 +59,6 @@ alias relogin='exec -l "$SHELL"'
 
 alias rmf='rm -fr'
 
-alias sudo='sudo '
+if type -t sudo &>/dev/null; then
+    alias sudo='sudo '
+fi
--- .bash_profile.d/exports.sh
+++ .bash_profile.d/exports.sh
@@ -1,9 +1,17 @@
-export GPG_TTY="$(tty)"
+if type -t gpg-agent tty &>/dev/null; then
+    export GPG_TTY="$(tty)"
+fi
 
 export HISTSIZE=100000
 export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 
-export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
-export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
+if ls -d --color=auto &>/dev/null || type -t gls &>/dev/null; then
+    export LS_COLORS="di=34:ln=36:so=35:pi=33:ex=31:bd=33:cd=33:su=30;41:sg=37;41:tw=30;44:ow=37;44"
+fi
+if ls -Gd &>/dev/null; then
+    export LSCOLORS="exgxfxdxbxdxdxabhbaehe"
+fi
 
-[[ ":$PATH:" == *:"$HOME/.dotfiles/bin":* ]] || export PATH+=":$HOME/.dotfiles/bin"
+if [[ ":$PATH:" != *:"$HOME/.dotfiles/bin":* ]]; then
+    export PATH+=":$HOME/.dotfiles/bin"
+fi
