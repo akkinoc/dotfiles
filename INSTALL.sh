91fa953cc29ad5cf41d7fa5920df50151905230f
Akihiro Kondo <akihyro@gmail.com>
2018-05-02 21:35:12 +0900

    Refactor bash scripts

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -eu -o pipefail
-cd "$(dirname "$0")"
 
-DOTFILES_HOME="$PWD"
+DOTFILES_HOME="$(cd "$(dirname "$0")" && pwd)"
 DOTFILES_DATA_DIR="$HOME/.dotfiles"
-DOTFILES_HIST_DIR="$DOTFILES_DATA_DIR/history/$(date +%Y%m%dT%H%M%S)"
+DOTFILES_HIST_DIR="$DOTFILES_DATA_DIR/history/$(date '+%Y%m%dT%H%M%S')"
+
+function ensure_dir {
+    local dir="$1" mode="${2:-}"
+    mkdir -p "$dir"
+    [[ -z "$mode" ]] || chmod "$mode" "$dir"
+}
 
 function link_dotfile {
-    local target="$1"
+    local target="$1" mode="${2:-}"
     local src_file="$DOTFILES_HOME/$target"
     local dest_file="$HOME/$target"
     local hist_file="$DOTFILES_HIST_DIR/$target"
@@ -17,8 +22,8 @@ function link_dotfile {
         mkdir -p "$(dirname "$hist_file")"
         mv "$dest_file" "$hist_file"
     fi
-    mkdir -p "$(dirname "$dest_file")"
     ln -s "$src_file" "$dest_file"
+    [[ -z "$mode" ]] || chmod "$mode" "$dest_file"
 }
 
 function report_result {
@@ -31,11 +36,14 @@ function report_result {
 }
 
 trap report_result EXIT
+
 link_dotfile .Brewfile
 link_dotfile .bash_completion
 link_dotfile .bash_profile
 link_dotfile .bash_profile.d
-link_dotfile .dotfiles/bin
 link_dotfile .gitattributes
 link_dotfile .gitconfig
 link_dotfile .gitignore
+
+ensure_dir .dotfiles
+link_dotfile .dotfiles/bin
