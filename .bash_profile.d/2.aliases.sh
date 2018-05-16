e1e35261793466e11077be749556f0cea3994be2
Akihiro Kondo <akihyro@gmail.com>
2018-05-17 07:51:36 +0900

    Define aliases that change existing commands first

--- .bash_profile.d/2.aliases.sh
+++ .bash_profile.d/2.aliases.sh
@@ -1,3 +1,39 @@
+if type -t grep &>/dev/null; then
+    alias grep='grep --color=auto'
+fi
+if type -t egrep &>/dev/null; then
+    alias egrep='egrep --color=auto'
+fi
+if type -t fgrep &>/dev/null; then
+    alias fgrep='fgrep --color=auto'
+fi
+if type -t ggrep &>/dev/null; then
+    alias ggrep='ggrep --color=auto'
+fi
+if type -t gegrep &>/dev/null; then
+    alias gegrep='gegrep --color=auto'
+fi
+if type -t gfgrep &>/dev/null; then
+    alias gfgrep='gfgrep --color=auto'
+fi
+
+if ls -d --color=auto &>/dev/null; then
+    alias ls='ls --color=auto'
+elif ls -Gd &>/dev/null; then
+    alias ls='ls -G'
+fi
+if type -t gls &>/dev/null; then
+    alias gls='gls --color=auto'
+fi
+
+if type -t sudo &>/dev/null; then
+    alias sudo='sudo '
+fi
+
+if type -t vim &>/dev/null; then
+    alias vi="vim"
+fi
+
 alias ~='cd ~'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -26,24 +62,6 @@ elif type -t git &>/dev/null; then
     alias g='git'
 fi
 
-if type -t grep &>/dev/null; then
-    alias grep='grep --color=auto'
-fi
-if type -t egrep &>/dev/null; then
-    alias egrep='egrep --color=auto'
-fi
-if type -t fgrep &>/dev/null; then
-    alias fgrep='fgrep --color=auto'
-fi
-if type -t ggrep &>/dev/null; then
-    alias ggrep='ggrep --color=auto'
-fi
-if type -t gegrep &>/dev/null; then
-    alias gegrep='gegrep --color=auto'
-fi
-if type -t gfgrep &>/dev/null; then
-    alias gfgrep='gfgrep --color=auto'
-fi
 if type -t ggrep &>/dev/null; then
     alias ge='ggrep -EIn --exclude-dir=.git'
 elif type -t grep &>/dev/null; then
@@ -52,14 +70,6 @@ fi
 
 alias hist='history'
 
-if ls -d --color=auto &>/dev/null; then
-    alias ls='ls --color=auto'
-elif ls -Gd &>/dev/null; then
-    alias ls='ls -G'
-fi
-if type -t gls &>/dev/null; then
-    alias gls='gls --color=auto'
-fi
 if type -t gls &>/dev/null; then
     alias l='gls -l'
     alias la='gls -al'
@@ -75,11 +85,3 @@ fi
 alias reload='exec -l "$SHELL"'
 
 alias rmf='rm -fr'
-
-if type -t sudo &>/dev/null; then
-    alias sudo='sudo '
-fi
-
-if type -t vim &>/dev/null; then
-    alias vi="vim"
-fi
