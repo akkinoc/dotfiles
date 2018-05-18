fa0669da54c7cb0acbbc9f1d29b4cc0adddf99db
Akihiro Kondo <akihyro@gmail.com>
2018-05-18 21:15:34 +0900

    Change alias quotes

--- .bash_profile.d/2.aliases.sh
+++ .bash_profile.d/2.aliases.sh
@@ -1,100 +1,100 @@
 if type -t colordiff &>/dev/null; then
-    alias diff='colordiff'
+    alias diff="colordiff"
 fi
 
 if type -t grep &>/dev/null; then
-    alias grep='grep --color=auto'
+    alias grep="grep --color=auto"
 fi
 if type -t egrep &>/dev/null; then
-    alias egrep='egrep --color=auto'
+    alias egrep="egrep --color=auto"
 fi
 if type -t fgrep &>/dev/null; then
-    alias fgrep='fgrep --color=auto'
+    alias fgrep="fgrep --color=auto"
 fi
 if type -t ggrep &>/dev/null; then
-    alias ggrep='ggrep --color=auto'
+    alias ggrep="ggrep --color=auto"
 fi
 if type -t gegrep &>/dev/null; then
-    alias gegrep='gegrep --color=auto'
+    alias gegrep="gegrep --color=auto"
 fi
 if type -t gfgrep &>/dev/null; then
-    alias gfgrep='gfgrep --color=auto'
+    alias gfgrep="gfgrep --color=auto"
 fi
 
 if ls -d --color=auto &>/dev/null; then
-    alias ls='ls --color=auto'
+    alias ls="ls --color=auto"
 elif ls -Gd &>/dev/null; then
-    alias ls='ls -G'
+    alias ls="ls -G"
 fi
 if type -t gls &>/dev/null; then
-    alias gls='gls --color=auto'
+    alias gls="gls --color=auto"
 fi
 
 if type -t sudo &>/dev/null; then
-    alias sudo='sudo '
+    alias sudo="sudo "
 fi
 
 if type -t vim &>/dev/null; then
-    alias ex='vim -e'
-    alias exim='vim -E'
-    alias rvim='vim -Z'
-    alias rview='vim -RZ'
-    alias vi='vim'
-    alias view='vim -R'
-    alias vimdiff='vim -d'
+    alias ex="vim -e"
+    alias exim="vim -E"
+    alias rvim="vim -Z"
+    alias rview="vim -RZ"
+    alias vi="vim"
+    alias view="vim -R"
+    alias vimdiff="vim -d"
 elif type -t vi &>/dev/null; then
-    alias ex='vi -e'
-    alias view='vi -R'
+    alias ex="vi -e"
+    alias view="vi -R"
 fi
 
-alias ~='cd ~'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias -- -='cd -'
+alias ~="cd ~"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias -- -="cd -"
 if type -t pwd-ext &>/dev/null; then
-    alias @='cd "$(pwd-ext)"'
+    alias @="cd \"\$(pwd-ext)\""
 fi
 
 if type -t diff &>/dev/null; then
-    alias d='diff -u'
+    alias d="diff -u"
 fi
 
 if type -t code &>/dev/null; then
-    alias e='code'
+    alias e="code"
 elif [[ -n "$EDITOR" ]]; then
-    alias e='$EDITOR'
+    alias e="\"$EDITOR\""
 fi
 
 if type -t hub &>/dev/null; then
-    alias g='hub'
+    alias g="hub"
 elif type -t git &>/dev/null; then
-    alias g='git'
+    alias g="git"
 fi
 
 if type -t ggrep &>/dev/null; then
-    alias ge='ggrep -EIn --exclude-dir=.git'
+    alias ge="ggrep -EIn --exclude-dir=.git"
 elif type -t grep &>/dev/null; then
-    alias ge='grep -EIn --exclude-dir=.git'
+    alias ge="grep -EIn --exclude-dir=.git"
 fi
 
 if type -t gls &>/dev/null; then
-    alias l='gls -Fl'
-    alias la='gls -Fal'
+    alias l="gls -Fl"
+    alias la="gls -Fal"
 else
-    alias l='ls -Fl'
-    alias la='ls -Fal'
+    alias l="ls -Fl"
+    alias la="ls -Fal"
 fi
 
 if type -t open-args-or-wd &>/dev/null; then
-    alias o='open-args-or-wd'
+    alias o="open-args-or-wd"
 fi
 
-alias reload='exec -l "$SHELL"'
+alias reload="exec -l \"$SHELL\""
 
-alias rmf='rm -fr'
+alias rmf="rm -fr"
 
 if type -t view &>/dev/null; then
-    alias v='view'
+    alias v="view"
 fi
