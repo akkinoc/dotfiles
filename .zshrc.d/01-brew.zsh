b029189b2dda436b031893dbbabd3348892fb54d
Akihiro Kondo <akkinoc@gmail.com>
2026-04-13 01:07:58 +0900

    Update Homebrew options

--- .zshrc.d/01-brew.zsh
+++ .zshrc.d/01-brew.zsh
@@ -3,3 +3,13 @@ if [[ -x /opt/homebrew/bin/brew ]]; then
 elif command -v brew &>/dev/null; then
     eval "$(brew shellenv)"
 fi
+
+if command -v brew &>/dev/null; then
+    export HOMEBREW_BUNDLE_DUMP_NO_CARGO=1
+    export HOMEBREW_BUNDLE_DUMP_NO_FLATPAK=1
+    export HOMEBREW_BUNDLE_DUMP_NO_GO=1
+    export HOMEBREW_BUNDLE_DUMP_NO_KREW=1
+    export HOMEBREW_BUNDLE_DUMP_NO_NPM=1
+    export HOMEBREW_BUNDLE_DUMP_NO_UV=1
+    export HOMEBREW_BUNDLE_DUMP_NO_VSCODE=1
+fi
